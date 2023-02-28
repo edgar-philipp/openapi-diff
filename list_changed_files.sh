a2b4231768ccc270034da8b6f81fe75f05170b4d
edgar-philipp <edgar.philipp@gmail.com>
2023-02-28 18:05:44 +0100

    Update list_changed_files.sh

--- list_changed_files.sh
+++ list_changed_files.sh
@@ -2,18 +2,13 @@
 
 echo "::group::List Changed Files"
 
-path_filters="$PATH_FILTERS"
-path_filters=${path_filters/*[/}                 # Remove everything up to and including [ sign
-path_filters=${path_filters/\]*/}                # Remove ] and everything after it
-path_filters=${path_filters//,/}                 # Remove all occurrences of , sign
-path_filters=$(echo "$path_filters" | tr \" \')  # Replace all double quotes by single quotes
-echo "Applying path filters: ${path_filters}"
+echo "Applying path filters: ${PATH_FILTERS}"
 
 echo "git log --oneline"
 echo $(git log --oneline)
 
 # filter M = modified
-command="git diff-tree --diff-filter=M --no-commit-id --name-only -r $CURRENT_HASH -- ${path_filters}"
+command="git diff-tree --diff-filter=M --no-commit-id --name-only -r $CURRENT_HASH -- ${PATH_FILTERS}"
 echo $command 
 list=$( eval $command)
 echo "Changed files: $list"
