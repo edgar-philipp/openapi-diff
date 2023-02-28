bf110bf5bf33129f7828ee81ea22703751e22005
edgar-philipp <edgar.philipp@gmail.com>
2023-02-28 18:29:59 +0100

    Update create_openapi_diff_cli.sh

--- create_openapi_diff_cli.sh
+++ create_openapi_diff_cli.sh
@@ -8,7 +8,7 @@ git clone https://github.com/OpenAPITools/openapi-diff.git .build
 
 cd .build/cli
 mvn clean install
-cp target/openapi-diff-cli-"$VERSION"-SNAPSHOT-all.jar "$BASE_DIR"/openapi-diff-cli-"$VERSION".jar
+cp target/openapi-diff-cli-"$VERSION"-SNAPSHOT-all.jar "$BASE_DIR"/scripts/openapi-diff-cli-"$VERSION".jar
 
 cd "$BASE_DIR"
 rm -rf .build
