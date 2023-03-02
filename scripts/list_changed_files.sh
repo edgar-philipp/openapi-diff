59888c2036c050353c9b823d21c4111ab5e5344a
edgar-philipp <edgar.philipp@gmail.com>
2023-03-02 16:16:15 +0100

    Update list_changed_files.sh

--- scripts/list_changed_files.sh
+++ scripts/list_changed_files.sh
@@ -1,17 +1,29 @@
 #!/bin/bash
 
-echo "::group::List Changed Files"
+if [ $OPERATION -eq "Added" ]; then
+  $flag="A"
+elif [ $OPERATION -eq "Deleted" ]; then
+  $flag="D"
+elif [ $OPERATION -eq "Modified" ]; then
+	$flag="M"
+elif [ $OPERATION -eq "Changed" ]; then
+	$flag="M"
+else 
+  echo "Usage: OPERATION must be one of ['Added', 'Deleted', 'Modified', 'Changed']"
+  exit(1)
+fi
+
+echo "::group::List ${OPERATION} Files"
 
 echo "Applying path filters: ${PATH_FILTERS}"
 
 echo "git log --oneline"
 echo $(git log --oneline)
 
-# filter M = modified
-command="git diff-tree --diff-filter=M --no-commit-id --name-only -r $CURRENT_HASH -- ${PATH_FILTERS}"
+command="git diff-tree --diff-filter=${flag} --no-commit-id --name-only -r $CURRENT_HASH -- ${PATH_FILTERS}"
 echo $command 
 list=$( eval $command)
-echo "Changed files: $list"
+echo "${OPERATION} files: $list"
 
 # Remove CarriageReturn and LineFeed (CR/LF):
 changed_files="${list[*]//$'\n'/ }"
