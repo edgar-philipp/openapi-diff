0b623ae54e3cde4d00064a375eeb5b2420ff0004
edgar-philipp <edgar.philipp@gmail.com>
2023-03-03 12:35:45 +0100

    Update openapi_diff.sh

--- scripts/openapi_diff.sh
+++ scripts/openapi_diff.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 
-function to_step_summary {
-  echo "Report saved:"
-  ls $1
-  echo "### Report Summary" >> $GITHUB_STEP_SUMMARY
+function file_to_summary {
   while read line; do
     echo "Line: ${line}"
     echo "${line}" >> $GITHUB_STEP_SUMMARY
   done < "$1"
-  echo "" >> $GITHUB_STEP_SUMMARY
+}
+
+function generate_report {
+  echo "Report saved:"
+  ls $1
+  echo "### Report Summary" >> $GITHUB_STEP_SUMMARY
+  file_to_summary $1
+}
+
+function generate_diff {
   echo "### Diff" >> $GITHUB_STEP_SUMMARY
-  echo "" >> $GITHUB_STEP_SUMMARY
-  echo "$DIFF" >> $GITHUB_STEP_SUMMARY
+  echo "```diff" >> $GITHUB_STEP_SUMMARY
+  file_to_summary $1
+  echo "```" >> $GITHUB_STEP_SUMMARY
 }
 
 echo "::group::OpenAPI Diff"
@@ -25,16 +32,19 @@ git show ${PREVIOUS_HASH}:${CHANGED_FILE} > ${CHANGED_FILE}_old.yml
 ls ${CHANGED_FILE}_old.yml
 
 echo "diff ${PREVIOUS_HASH} ${CURRENT_HASH} -- ${CHANGED_FILE}"
-git diff ${PREVIOUS_HASH} ${CURRENT_HASH} -- ${CHANGED_FILE} >> $DIFF
+git diff ${PREVIOUS_HASH} ${CURRENT_HASH} -- ${CHANGED_FILE} > ${CHANGED_FILE}.diff
 
 echo "Check for breaking changes"
 report="Summary.md"
 java -jar openapi-diff-cli-2.1.0.jar ${CHANGED_FILE}_old.yml ${CHANGED_FILE}_new.yml --fail-on-incompatible --markdown ${report}
 
 if [ $? -ne 0 ]; then
-  echo "::error::Breaking changes on ${CHANGED_FILE}" 
+  echo "::error::Breaking changes on ${CHANGED_FILE}"
+  generate_report ${report}
+  gererate_diff ${CHANGED_FILE}.diff
+  exit 1
 fi
 
-to_step_summary ${report}
+generate_report ${report}
 
 echo "::endgroup::"
