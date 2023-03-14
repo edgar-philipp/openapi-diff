a3cdb48a68440fa1a7060676fa192844f9a2a0bd
edgar-philipp <edgar.philipp@gmail.com>
2023-03-14 08:59:32 +0100

    Update add_custom_spectral_rules.sh

--- scripts/add_custom_spectral_rules.sh
+++ scripts/add_custom_spectral_rules.sh
@@ -5,8 +5,8 @@ custom_rules=`echo "$1" | tr -d ,{} | tr '\n' $'\n'`
 custom_rules="rules:${custom_rules/'\n'}"
 echo "${custom_rules}"
 
-cp ./spectral_template.yml ./spectral_generated.yml
+cp ./spectral_template.yml ./spectral.yml
 
-result=$(CUSTOM_RULES=${custom_rules} envsubst < spectral_generated.yml)
-echo "$result" > spectral_generated.yml
+result=$(CUSTOM_RULES=${custom_rules} envsubst < spectral.yml)
+echo "$result" > spectral.yml
 echo $'Result:\n'"$result"
