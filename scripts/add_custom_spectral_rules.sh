1a7f3d925e2f4d1c7b5289c6f0eff7185118d5ae
edgar-philipp <edgar.philipp@gmail.com>
2023-03-14 09:21:54 +0100

    Update add_custom_spectral_rules.sh

--- scripts/add_custom_spectral_rules.sh
+++ scripts/add_custom_spectral_rules.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
-# expects input arg: json with custom rules
-custom_rules=`echo "$1" | tr -d ,{} | tr '\n' $'\n'`
+# expects json with custom rules
+custom_rules=`echo "$JSON_INPUT" | tr -d ,{} | tr '\n' $'\n'`
 custom_rules="rules:${custom_rules/'\n'}"
 echo "${custom_rules}"
 
-pwd
 linter_dir=${GITHUB_WORKSPACE}/workflow-repo/linter
 cp ${linter_dir}/spectral_template.yml ${linter_dir}/spectral.yml
 
+echo "Before:"
+less ${linter_dir}/spectral.yml
+
 result=$(CUSTOM_RULES=${custom_rules} envsubst < ${linter_dir}/spectral.yml)
 echo "$result" > ${linter_dir}/spectral.yml
-echo $'Result:\n'"$result"
+
+echo "After:"
+less ${linter_dir}/spectral.yml
