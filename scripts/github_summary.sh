3e87089ded04fb0c9fa7b7e9cb1b6f7af6e6e529
edgar-philipp <edgar.philipp@gmail.com>
2023-03-15 09:21:49 +0100

    Update github_summary.sh

--- scripts/github_summary.sh
+++ scripts/github_summary.sh
@@ -1,1 +1,21 @@
+#!/bin/bash
 
+function file_to_summary {
+  file=$1
+  OLD_IFS="$IFS"
+  IFS=
+  while read line; do
+    echo "${line}" >> $GITHUB_STEP_SUMMARY
+  done < "$file"
+  IFS="$OLD_IFS"
+}
+
+function create_section {
+  section_title=$1
+  file=$2
+  format=$3
+  echo "#### $section_title" >> $GITHUB_STEP_SUMMARY
+  echo '```$format' >> $GITHUB_STEP_SUMMARY
+  file_to_summary $file
+  echo '```' >> $GITHUB_STEP_SUMMARY
+}
