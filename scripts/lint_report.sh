0d78d91386ded54ae38d1262854bcb1eba1f54db
edgar-philipp <edgar.philipp@gmail.com>
2023-03-15 14:11:54 +0100

    Update lint_report.sh

--- scripts/lint_report.sh
+++ scripts/lint_report.sh
@@ -2,12 +2,12 @@
 
 echo "::group::Lint Report"
 
+linter_dir=${GITHUB_WORKSPACE}/workflow-repo/linter
+source ${GITHUB_WORKSPACE}/workflow-repo/scripts/github_summary.sh
+
 echo "Lint report saved:"
 ls ${LINT_RESULT}
 echo "### Lint report" >> $GITHUB_STEP_SUMMARY
-
-linter_dir=${GITHUB_WORKSPACE}/workflow-repo/linter
-source ${GITHUB_WORKSPACE}/workflow-repo/scripts/github_summary.sh
 
 create_section "Spectral file" ${linter_dir}/spectral.yml yaml
 create_section "Results" ${LINT_RESULT}
